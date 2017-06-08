02fb89551ce4005db3e3b7862c9c29217e5d6ae5
Gustavo Carvalho <gustavo.bio@gmail.com>
2017-06-08 11:58:46 -0300

    New search fields

--- R/get_images.R
+++ R/get_images.R
@@ -1,6 +1,8 @@
 #' Download images from the Reflora Website.
 #'
-#' @param taxon A unique taxon name. Can be anything (species, family, order).
+#' @param any A search query to be matched against all fields.
+#' @param family Family name.
+#' @param institution_code An institution code.
 #'
 #' @return \code{get_base_url} Returns the base url for a given taxon. Not very
 #' useful by itself. \code{get_number_of_images} returns the number of images available
@@ -14,13 +16,21 @@
 #' get_images(taxon)
 #' }
 #' @export
-get_images <- function(taxon) {
-  image_urls <- get_image_urls(taxon)
+get_images <- function(any = NULL, family = NULL, institution_code = NULL) {
+  check_args(any, family, institution_code)
+  image_urls <- get_image_urls(any, family, institution_code)
+  if (is.null(image_urls)) {
+    message("There aren't any images for the provided query.")
+    return(NULL)
+  }
   image_names <-
     regmatches(image_urls,
                regexpr("(?<=/imagecode/)(.*)(?=/size)", image_urls, perl = T))
-  if (!dir.exists(taxon))
-    dir.create(taxon)
+  path <- paste(any, family, institution_code, sep = "_")
+  path <- gsub("\\_{2}", "_", path)
+  path <- gsub("^\\_+|\\_+$", "", path)
+  if (!dir.exists(path))
+    dir.create(path)
   cat("Downloading", length(image_urls), "images:\n")
   pb <- txtProgressBar(min = 0, max = length(image_urls), style = 3)
   for (i in seq_along(image_urls)) {
@@ -34,7 +44,7 @@ get_images <- function(taxon) {
         # print(temp)
         setTxtProgressBar(pb, i)
         download.file(temp,
-                      destfile = paste0(paste0(taxon, "/"), image_names[i], ".jpg"),
+                      destfile = paste0(paste0(path, "/"), image_names[i], ".jpg"),
                       mode = "wb", quiet = TRUE)
       })
       if (!inherits(success, "try-error")) break
@@ -42,19 +52,26 @@ get_images <- function(taxon) {
     }
   }
   close(pb)
-  cat("Images downloaded to", paste0(getwd(), "/", taxon, "/\n"))
+  cat("Images downloaded to", paste0(getwd(), "/", path, "/\n"))
 }
 
 #' Get the base url for a taxon
 #'
-#' @param taxon A unique taxon name.
+#' @param any A search query to be matched against all fields.
+#' @param family Family name.
+#' @param institution_code An institution code.
 #' @param offset Page offset
 #' @export
-get_base_url <- function(taxon, offset = 0) {
+get_base_url <- function(any = NULL, family = NULL, institution_code = NULL, offset = 0) {
+  check_args(any, family, institution_code)
   URLencode(
     paste(
       "http://www.splink.org.br/showImages?ts_any=",
-      taxon,
+      any,
+      "&ts_family=",
+      family,
+      "&ts_institutioncode=",
+      institution_code,
       "&extra=withImages&search_id=2&search_seq=8&size=thumb&offset=",
       offset,
       sep = ""
@@ -64,10 +81,13 @@ get_base_url <- function(taxon, offset = 0) {
 
 #' Get the number of images for a given taxon
 #'
-#' @param taxon A unique taxon name
+#' @param any A search query to be matched against all fields.
+#' @param family Family name.
+#' @param institution_code An institution code.
 #' @export
-get_number_of_images <- function(taxon) {
-  n <- read_html(get_base_url(taxon)) %>%
+get_number_of_images <- function(any = NULL, family = NULL, institution_code = NULL) {
+  check_args(any, family, institution_code)
+  n <- read_html(get_base_url(any, family, institution_code)) %>%
     html_node("b") %>%
     html_text()
   regexp <- gregexpr("\\d+", n, perl = T)
@@ -76,16 +96,20 @@ get_number_of_images <- function(taxon) {
 
 #' Get the urls for a taxon
 #'
-#' @param taxon A unique taxon name
+#' @param any A search query to be matched against all fields.
+#' @param family Family name.
+#' @param institution_code An institution code.
 #' @export
-get_image_urls <- function(taxon) {
-  n <- get_number_of_images(taxon)
+get_image_urls <- function(any = NULL, family = NULL, institution_code = NULL) {
+  check_args(any, family, institution_code)
+  n <- get_number_of_images(any, family, institution_code)
+  if (n == 0) return(NULL)
   links <- NULL
   for (i in 1:floor(n/100)) {
     tries <- 1
     repeat {
       if (tries > 3) stop("Can't reach the website. Please try again later.")
-      link <- try(read_html(get_base_url(taxon, offset = i * 100)) %>%
+      link <- try(read_html(get_base_url(any, family, institution_code, offset = i * 100)) %>%
                     html_nodes(xpath = '//a[@class="highslide"]') %>%
                     html_attr(name = "href"))
       if (!inherits(link, "try-error")) break
@@ -96,3 +120,6 @@ get_image_urls <- function(taxon) {
   links
 }
 
+check_args <- function(any, family, institution_code) {
+  if (is.null(any) & is.null(family) & is.null(institution_code)) stop("Please provide at least one search field.")
+}
